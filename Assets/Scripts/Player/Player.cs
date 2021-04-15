b9257e33f0e909e111c0e2b967490912c89e12f8
ckh2444 <ckh2445@naver.com>
2021-04-15 21:39:07 +0900

    2021 - 04 - 15 (광호)
    1. Enemy를 전부 현재 Enemy_Dish로 수정 및 폴더 생성 -> 차 후 작업을 편하기 하기 위해 분할
    (1). Assets - Animations - Enemy_Dish_Stay, Enemy_Dish_Attack
    (2). Assets - prefab - Enemy -> Enemy_Dish 이름 변경
    (3). Assets - Scripts - Enemy - Enemy_Dish 폴더 생성
         -. Assets - Scripts - Enemy에서의 Enemy.cs -> Enemy_Dish.cs 이름 변경
         -. Assets - Scripts - Enemy에서의 Enemy_combat.cs -> Enemy_Dish_Combat.cs 이름 변경
         Enemy_Dish 폴더안에 Enemy_Dish.cs , Enemy_Dish_Combat.cs 넣어줌
    (4). Assets - Sprites - Enemy - Enemy_Dish 폴더 생성
         -. Asseets - Sprites - Enemy폴더에있는 monster1_attack , monster1_Stay를  Dish_Attack,
            Dish_Stay로 이름을 바꾼 후 Enemy_Dish 폴더안에 넣어줌
    
    2. Assets - Scripts - Player -  Player.cs에 TakeDamage() 함수 작성
    
    3. Assets - Scripts - Enemy - Enemy_Dish - Enemy_Dish_Combat.cs에 여러가지 손봄
     -> 애니메이션에 함수를 적용하기위해 Attack 함수를 만듬

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,28 +5,20 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public int Player_Maxhealth = 100; // 플레이어의 체력
-    public static int Player_Currenthealth; // 플레이어의 현재 체력 
+    public int Player_Currenthealth; // 플레이어의 현재 체력 
 
     public HealthBar Player_HealthBar;//체력바를 추가하기위한 그것에 대한 참조를 만들어야함  그래서 공중 보건 바를 만듬 
 
     
-    void Start()
+    private void Start()
     {
         Player_Currenthealth = Player_Maxhealth;  //현재 체력을 나의 maxHealth로 저장
         Player_HealthBar.SetMaxHealth(Player_Maxhealth); //HP를 현재 나의 최대 최력치에 저장한다.
     }
 
-    // Update is called once per frame
-    /*void Update()
-    {
-        Player_Currenthealth = Player_Maxhealth;
-        Player_HealthBar.SetHealth(Player_Currenthealth);
-    }*/
-
-
     public void TakeDamage() //대미지를 입을 함수 
     {
-        Player_Currenthealth -= EnemyCombat.Enemy_Damage; // 현재체력을 damage 매개변수를 받아 그만큼 감소 
+        Player_Currenthealth -= Enemy_Dish_Combat.Enemy_Dish_Damage; // 현재체력을 damage 매개변수를 받아 그만큼 감소 
         Player_HealthBar.SetHealth(Player_Currenthealth); // 감소한피를 적용 
 
         if(Player_Currenthealth <= 0)
@@ -35,8 +27,10 @@ public class Player : MonoBehaviour
         }
     }
 
-    void Die()
+    private void Die() //test 쓸모없음
     {
         Debug.Log("Player died");
+
     }
+   
 }
