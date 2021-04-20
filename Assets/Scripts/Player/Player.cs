faff1a576e01cdd04a0efe7344bdb535745d3b02
PrintedLove <dnjswn0328@gmail.com>
2021-04-20 11:13:22 +0900

    2021-04-20 광호형 PR(현재 이슈 수정 X)
    캐릭터 적 닿을시 공격 안함.
    데미지 애니메이션 연결 안됨.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -8,8 +8,8 @@ public class Player : MonoBehaviour
     public int Player_Currenthealth; // 플레이어의 현재 체력 
 
     public HealthBar Player_HealthBar;//체력바를 추가하기위한 그것에 대한 참조를 만들어야함  그래서 공중 보건 바를 만듬 
+    public GameObject Player_DamageDisplay; // 맞은 데미지 표시를 위한 변수 
 
-    
     private void Start()
     {
         Player_Currenthealth = Player_Maxhealth;  //현재 체력을 나의 maxHealth로 저장
@@ -21,7 +21,10 @@ public class Player : MonoBehaviour
         Player_Currenthealth -= Enemy_Dish_Combat.Enemy_Dish_Damage; // 현재체력을 damage 매개변수를 받아 그만큼 감소 
         Player_HealthBar.SetHealth(Player_Currenthealth); // 감소한피를 적용 
 
-        if(Player_Currenthealth <= 0)
+        GameObject Player_Display = Instantiate(Player_DamageDisplay, transform.position, Quaternion.identity);
+        Player_Display.transform.GetChild(0).GetComponent<TextMesh>().text = Enemy_Dish_Combat.Enemy_Dish_Damage.ToString();
+
+        if (Player_Currenthealth <= 0)
         {
             Die();
         }
