9af2e4c201c717d00863b12290a2da3bba447485
wnsfus123 <79974616+wnsfus123@users.noreply.github.com>
2021-05-26 14:12:40 +0900

    메인화면 , 스테이지선택 , 추가
    kakao 참고

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,11 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public GameObject menuSet;
+    public GameObject menuSet2;
+    public GameObject Main;
+    public GameObject ingame;
+    public GameObject SB;
+    public GameObject SS;
 
 
     // Start is called before the first frame update
@@ -23,6 +28,22 @@ public class GameManager : MonoBehaviour
                 menuSet.SetActive(false);
             else
                 menuSet.SetActive(true);
+        }
+
+        if (Input.GetButtonDown("Enter")) //ESC 메뉴 출력
+        {
+            if (Main.activeSelf) //ESC로 켜고 끄기 가능하도록
+                Main.SetActive(false);
+                ingame.SetActive(true);
+            // else
+            //     Main.SetActive(true);
+        }
+
+        if (Input.GetButtonDown("SB")) //ESC 메뉴 출력
+        {
+            if (SB.activeSelf) //ESC로 켜고 끄기 가능하도록
+                ingame.SetActive(false);
+                SS.SetActive(true);
         }
 
     }
